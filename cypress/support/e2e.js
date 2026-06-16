97ce8fe52d92d3f5a4453b823ade83a9e7d14ec1
Caio Soares <caioadriana5@gmail.com>
2026-06-15 22:27:35 -0300

    cypress slower

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,5 @@
 // Suporte global dos testes E2E do front.
 // Os cenários usam cy.intercept() para validar a interface sem depender do backend.
+import {slowCypressDown} from 'cypress-slow-down';
+
+slowCypressDown(200);
